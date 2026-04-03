db5d58892dde641c5c77dddad6686713ca383113
L1sa620 <lisevbom@gmail.com>
2026-04-03 22:40:57 +0300

    Refactor vector

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,23 +1,20 @@
 #ifndef TOP_IT_VECTOR_HPP
 #define TOP_IT_VECTOR_HPP
+
 #include <cassert>
 #include <cstddef>
 #include <utility>
+
 namespace topit
 {
   template< class T >
   struct Vector
   {
-  private:
-    T *data_;
-    size_t size_, capacity_;
-
-    explicit Vector< T >(size_t k);
-  public:
     Vector< T >();
     Vector< T >(const Vector< T >&);
     Vector< T >& operator=(const Vector< T >&);
     ~Vector< T >();
+
     T& operator[](size_t id) noexcept;
     const T& operator[](size_t id) const noexcept;
     size_t getSize() const noexcept;
@@ -27,13 +24,20 @@ namespace topit
     void pushFront(const T&);
     void pop_back();
     void swap(Vector< T >& rhs) noexcept;
+
+  private:
+    T *data_;
+    size_t size_, capacity_;
+
+    explicit Vector< T >(size_t k);
+    void pushBackImpl(const T&);
   };
 }
 
 template< class T >
 topit::Vector< T >::Vector(size_t k):
-  data_(new T[k]),
-  size_(k),
+  data_(static_cast< T * >(::operator new(sizeof(T) * k))),
+  size_(0),
   capacity_(k)
 {}
 
@@ -46,47 +50,55 @@ topit::Vector< T >::Vector():
 
 template< class T >
 topit::Vector< T >::Vector(const Vector< T >& rhs):
-  topit::Vector< T >::Vector(rhs.getSize())
+  Vector(rhs.getSize())
 {
-  for (size_t i = 0; i < getSize(); i++)
+  for (size_t i = 0; i < rhs.getSize(); ++i)
   {
-    data_[i] = rhs[i];
+    pushBackImpl(rhs[i]);
   }
 }
 
 template< class T >
-topit::Vector< T >::~Vector< T >()
+topit::Vector< T >::~Vector()
 {
-  delete [] data_;
+  for (size_t i = 0; i < size_; ++i)
+  {
+    data_[i].~T();
+  }
+  ::operator delete(data_);
 }
 
 template< class T >
 topit::Vector< T >& topit::Vector< T >::operator=(const Vector< T >& rhs)
 {
-  Vector< T > copy(rhs);
-  swap(copy);
+  if (this == std::addressof(rhs))
+  {
+    return *this;
+  }
+  Vector< T > cpy(rhs);
+  swap(cpy);
   return *this;
 }
 
 template< class T >
 void topit::Vector< T >::swap(Vector< T >& rhs) noexcept
 {
-  std::swap(rhs.data_, data_);
-  std::swap(rhs.capacity_, capacity_);
-  std::swap(rhs.size_, size_);
+  std::swap(data_, rhs.data_);
+  std::swap(size_, rhs.size_);
+  std::swap(capacity_, rhs.capacity_);
 }
 
 template< class T >
 T& topit::Vector< T >::operator[](size_t id) noexcept
 {
-  assert(id < getSize());
-  return data_[id];
+  const Vector< T > *cthis = this;
+  return const_cast< T& >((*cthis)[id]);
 }
 
 template< class T >
 const T& topit::Vector< T >::operator[](size_t id) const noexcept
 {
-  assert(id < getSize());
+  assert(id < size_);
   return data_[id];
 }
 
@@ -109,33 +121,41 @@ bool topit::Vector< T >::isEmpty() const noexcept
 }
 
 template< class T >
+void topit::Vector< T >::pushBackImpl(const T& val)
+{
+  new (data_ + size_) T(val);
+  ++size_;
+}
+
+template< class T >
 void topit::Vector< T >::pushBack(const T& val)
 {
-  Vector< T > copy(getSize() + 1);
-  for (size_t i = 0; i < getSize(); i++)
+  Vector< T > cpy(getSize() + 1);
+  for (size_t i = 0; i < getSize(); ++i)
   {
-    copy[i] = (*this)[i];
+    cpy.pushBackImpl(data_[i]);
   }
-  copy[getSize()] = val;
-  swap(copy);
+  cpy.pushBackImpl(val);
+  swap(cpy);
 }
 
 template< class T >
 void topit::Vector< T >::pushFront(const T& val)
 {
-  Vector< T > copy(getSize() + 1);
-  copy[0] = val;
-  for (size_t i = 0; i < getSize(); i++)
+  Vector< T > cpy(getSize() + 1);
+  cpy.pushBackImpl(val);
+  for (size_t i = 0; i < getSize(); ++i)
   {
-    copy[i + 1] = (*this)[i];
+    cpy.pushBackImpl(data_[i]);
   }
-  swap(copy);
+  swap(cpy);
 }
 
 template< class T >
 void topit::Vector< T >::pop_back()
 {
   assert(size_ > 0);
-  size_--;
+  data_[--size_].~T();
 }
+
 #endif
